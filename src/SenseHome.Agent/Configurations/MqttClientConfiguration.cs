86e38b5aad109c4d2ef6349e001843dc714af51a
Rafiul Islam <avoidcloud@gmail.com>
2020-10-23 10:26:37 +0600

    injected hosted mqtt managed client with required services

---
+++ src/SenseHome.Agent/Configurations/MqttClientConfiguration.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MQTTnet.Client.Options;
+using MQTTnet.Extensions.ManagedClient;
+using SenseHome.Agent.Options;
+using SenseHome.Agent.Services;
+using SenseHome.Agent.Settings;
+
+namespace SenseHome.Agent.Configurations
+{
+    public static class MqttClientConfiguration
+    {
+        public static void AddHostedMqttClient(this IServiceCollection services, IConfiguration configuration)
+        {
+            var brokerSettings = new MqttBrokerSettings();
+            configuration.GetSection(nameof(MqttBrokerSettings)).Bind(brokerSettings);
+            services.AddConfiguredMqttClientService(optionBuilder =>
+            {
+                optionBuilder
+                .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
+                .WithClientOptions(new MqttClientOptionsBuilder()
+                    .WithClientId("")
+                    .WithCredentials("", "")
+                    .WithTcpServer(brokerSettings.Host, brokerSettings.Port)
+                    .Build());
+            });
+        }
+
+        private static IServiceCollection AddConfiguredMqttClientService(this IServiceCollection services,
+                    Action<AspCoreManagedMqttClientOptionBuilder> configuration)
+        {
+            services.AddSingleton<IManagedMqttClientOptions>(serviceProvider =>
+            {
+                var optionsBuilder = new AspCoreManagedMqttClientOptionBuilder(serviceProvider);
+                configuration(optionsBuilder);
+                return optionsBuilder.Build();
+            });
+            services.AddSingleton<MqttClientService>();
+            services.AddSingleton<IHostedService>(serviceProvider =>
+            {
+                return serviceProvider.GetService<MqttClientService>();
+            });
+            services.AddTransient<MqttClientServiceProvider>(serviceProvider =>
+            {
+                var mqttClientService = serviceProvider.GetService<MqttClientService>();
+                var mqttClientServiceProvider = new MqttClientServiceProvider(mqttClientService);
+                return mqttClientServiceProvider;
+            });
+            return services;
+        }
+    }
+}
